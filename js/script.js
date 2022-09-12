5db4909b0c32faaf1776d5103f3146d4f7f94710
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-09-12 14:35:46 +0200

    push

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log('JS OK')
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            text: 'Hello World',
+            image: 'https://picsum.photos/300/200',
+        }
+    }
+);
